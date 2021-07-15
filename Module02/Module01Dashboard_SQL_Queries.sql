c0cd933790516c63570b6e2e254c5f5189673454
VolkovMAbiz <77934973+VolkovMAbiz@users.noreply.github.com>
2021-07-15 20:53:38 +0300

    Update Module01Dashboard_SQL_Queries.sql

--- Module02/Module01Dashboard_SQL_Queries.sql
+++ Module02/Module01Dashboard_SQL_Queries.sql
@@ -1,25 +1,48 @@
-Total Sales
+-- Total Sales
+select sum(sales) from orders
 
-Total Profit
+--Total Profit
+select sum(profit) from orders
 
-Profit Ratio
+--Profit Ratio
+select sum(profit)/sum(sales) "Gross profit margin" from orders
 
-Profit per Order
+--Profit per Order
+select sum(profit)/(select count(distinct order_id) from orders) "Profit per order" from orders
 
-Sales per Customer
+--Sales per Customer
+select sum(sales)/(select count(distinct customer_id) from orders) "Sales per customer" from orders
 
-Avg. Discount
+--Avg. Discount
+select avg(discount) from orders
 
-Monthly Sales by Segment
+--Monthly Sales by Segment
+select sum(sales),segment, date_trunc('month',order_date) "month" from orders 
+group by segment,month
 
-Monthly Sales by Product Category
+--Monthly Sales by Product Category
+select sum(sales),category, date_trunc('month',order_date) "month" from orders
+group by category,month
 
-Sales by Product Category over time
+--Sales by Product Category over time
+select sum(sales),category from orders
+group by category
 
-Sales and Profit by Customer
+--Sales and Profit by Customer
+select sum(sales)/(select count(distinct customer_id) from orders) "sales per customer",sum(profit)/(select count(distinct customer_id) from orders) "profit per customer" from orders
 
-Sales per region
+--Sales per region
+select sum(sales),region from orders 
+group by region
 
-Returned orders
+--Returned orders
+select count(distinct a.order_id) from orders a left join returns b on a.order_id = b.order_id
+where b.returned is not null
 
-Profitable orders
+--Returned profit
+select sum(profit) from orders a left join returns b on a.order_id = b.order_id
+where b.returned is not null
+
+--Profitable orders
+select count(distinct order_id) "Orders", 100.0 * count(distinct order_id)/(select count(distinct order_id) from orders)  "Profitable orders %" from orders
+where profit >0
