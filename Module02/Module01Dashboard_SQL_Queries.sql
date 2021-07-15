d7388e71bbf1a573754961618d128695fbec4ea8
VolkovMAbiz <77934973+VolkovMAbiz@users.noreply.github.com>
2021-07-15 16:41:23 +0300

    Create Module01Dashboard_SQL_Queries.sql

---
+++ Module02/Module01Dashboard_SQL_Queries.sql
@@ -0,0 +1,25 @@
+Total Sales
+
+Total Profit
+
+Profit Ratio
+
+Profit per Order
+
+Sales per Customer
+
+Avg. Discount
+
+Monthly Sales by Segment
+
+Monthly Sales by Product Category
+
+Sales by Product Category over time
+
+Sales and Profit by Customer
+
+Sales per region
+
+Returned orders
+
+Profitable orders
